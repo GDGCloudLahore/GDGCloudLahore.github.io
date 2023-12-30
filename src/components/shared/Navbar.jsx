269f726c3197a8c1f86a8529580db6ff568a9109
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2023-12-31 02:39:35 +0500

    :hammer: Feat - Worked on active route

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,8 +1,8 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
 
 const Navbar = () => {
   const pathname = usePathname();
