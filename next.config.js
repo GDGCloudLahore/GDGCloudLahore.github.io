b6c53582672d463242a8cab5877f7ac43e90aef0
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2024-01-22 23:59:30 +0500

    :bug: Image bug fixing

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        port: "",
+      },
     ],
   },
 };
