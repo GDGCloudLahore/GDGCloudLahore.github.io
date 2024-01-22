ad6b0879544c501e2fdcc99815c364f738c6fd84
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-01-22 23:37:46 +0500

    Update mentors.js

--- src/mocks/mentors.js
+++ src/mocks/mentors.js
@@ -14,16 +14,16 @@ export const mentorTags = [
 ];
 
 const allMentors = [
-    // {
-    //     image: "/assets/team/adil.jpg",
-    //     name: "Syed Asad Ali",
-    //     skill: "DevOps Engineer",
-    //     bio: "CNCF Ambassador | GDG Organizer",
-    //     tag: mentorTags[0],
-    //     topmate: "",
-    //     linkedin: ""
-    // },
+     {
+         image: "https://avatars.githubusercontent.com/u/53600644?s=400&u=a570538518ba0a8c51dab1a5a9c72852bf05cbde&v=4",
+         name: "Adil Shahzad",
+         skill: "DevOps Engineer",
+         bio: "Adil Shahzad, a multi-cloud certified Senior DevOps Engineer at Strategics Systems International, has spoken at Amazon Web Services and contributed to Pakt Publishing. He's also organized over 10 tech events in Pakistan, significantly empowering the tech community there. ",
+         tag: mentorTags[1], mentorTags[2]
+         topmate: "https://topmate.io/shahzad",
+         linkedin: "https://www.linkedin.com/in/adilshehzad7/"
+    },
   ];
   
   export default allMentors;
-  
+   
