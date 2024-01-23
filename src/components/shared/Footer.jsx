5775e1212bea39abe8fe68c2bd3d2a8ac3ac8c0d
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2024-01-23 21:48:05 +0500

    :hammer: Worked on footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
   return (
     <footer>
       <Container>
-        <div className="w-full h-[60px] bg-black rounded-[12px] flex justify-center items-center">
-          <p className="text-[18px] text-white font-normal">Developed by <Link target="_blank" href="https://www.linkedin.com/in/hasnaat-iftikhar" className="text-yellow font-medium">Hasnaat Iftikhar</Link></p>
+        <div className="w-full h-[60px] bg-black rounded-[12px] flex justify-center items-center px-[24px] gap-[8px]">
+        <p className="text-[16px] text-white font-normal">Developed by <Link target="_blank" href="https://www.hasnaat-iftikhar.com/" className="text-yellow font-medium">Hasnaat Iftikhar</Link>. Need any help? <Link target="_blank" href="https://www.linkedin.com/in/hasnaat-iftikhar" className="text-yellow font-medium">Click here</Link></p>
         </div>
         <div className="mt-[24px] w-full flex flex-col sm:flex-row justify-start sm:justify-between items-start sm:items-center gap-[40px] sm:gap-0 mb-[16px] sm:mb-0">
           <Logo />
