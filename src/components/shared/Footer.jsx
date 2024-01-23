ea4a6aaf4019d2362c1532978baecbdd0b2a1d0e
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2024-01-23 21:45:11 +0500

    :tada: Worked on footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -7,7 +7,10 @@ const Footer = () => {
   return (
     <footer>
       <Container>
-        <div className="w-full flex flex-col sm:flex-row justify-start sm:justify-between items-start sm:items-center gap-[40px] sm:gap-0 mb-[16px] sm:mb-0">
+        <div className="w-full h-[60px] bg-black rounded-[12px] flex justify-center items-center">
+          <p className="text-[18px] text-white font-normal">Developed by <Link target="_blank" href="https://www.linkedin.com/in/hasnaat-iftikhar" className="text-yellow font-medium">Hasnaat Iftikhar</Link></p>
+        </div>
+        <div className="mt-[24px] w-full flex flex-col sm:flex-row justify-start sm:justify-between items-start sm:items-center gap-[40px] sm:gap-0 mb-[16px] sm:mb-0">
           <Logo />
           <Link
             href="https://events.linuxfoundation.org/about/code-of-conduct/"
