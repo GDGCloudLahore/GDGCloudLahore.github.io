6f2d3f71244f48de733ef997253a6dcbb39e1362
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-01-30 13:44:04 +0500

    Update mentors.js

--- src/mocks/mentors.js
+++ src/mocks/mentors.js
@@ -10,6 +10,19 @@ export const mentorTags = [
     {
         name: "Cloud expert",
         value: "cloud-expert"
+    },
+    {
+        name: "Full Stack",
+        value: "full-stack"
+    },
+    {
+        name: "AI/ML",
+        value: "AI/ML"
+    },
+    {
+        name: "GCP Expert"
+        value: "gcp-expert"
+
     }
 ];
 
@@ -27,7 +40,7 @@ const allMentors = [
          name: "Hasnaat Iftikhar",
          skill: "FullStack Developer",
          bio: "Hasnaat is a versatile FullStack Developer who‘s passionate about creating seamless User Experiences. With over 5 years of experience, he excels in both frontend and backend development. He enjoys building on the web and regularly updates his skills by learning from documentation and articles.",
-         tags: [mentorTags[1]],
+         tags: [mentorTags[3]],
          topmate: "https://topmate.io/hasnaat_iftikhar",
     },
     {
@@ -45,7 +58,25 @@ const allMentors = [
          bio: "As a Multi-Cloud Certified Cloud DevOps Engineer, I specialize in AWS, Azure, and Google Cloud. My experience spans over 3 years in various projects where I’ve applied my expertise in CI/CD pipelines, automation, cloud infrastructure design, and deployment.",
          tags: [mentorTags[1], mentorTags[2]],
          topmate: "https://topmate.io/ahmed_tm",
-    }
+    },
+    {
+         image: "https://media.licdn.com/dms/image/C4D03AQGkpXv16rag1A/profile-displayphoto-shrink_400_400/0/1653203888415?e=1712188800&v=beta&t=0OXvVpjtrKcZXc0MlAzwJ8rgGlGZHm3dnY-DpMZ_OI4",
+         name: "Ahmed Tariq",
+         skill: "DevOps Engineer",
+         bio: "As a Multi-Cloud Certified Cloud DevOps Engineer, I specialize in AWS, Azure, and Google Cloud. My experience spans over 3 years in various projects where I’ve applied my expertise in CI/CD pipelines, automation, cloud infrastructure design, and deployment.",
+         tags: [mentorTags[1], mentorTags[2]],
+         topmate: "https://topmate.io/ahmed_tm",
+    },
+
+    {
+         image: "https://media.licdn.com/dms/image/D4D35AQF8FPalgwFz5g/profile-framedphoto-shrink_400_400/0/1694331469482?e=1707210000&v=beta&t=-tLlQuOOGg7JM6e-zdqZhRDJGjxyfVmqcJ7OdhySxyA",
+         name: "Muhammad Zunnurain Hussain",
+         skill: "GCP Expert",
+         bio: "Proven Network & Security specialist with comprehensive lifecycle advisory, client development, management, & technology consulting for fortune 100 clients. Strong client service with documented success cultivating relationships with senior clients across multiple advisory functions, resulting in the successful completion of projects, large-scale enterprise strategy implementation, & organization-wide transformations. Technical expertise presenting complex & technical terms to non-technical decision-makers and end-users. My passion lies in the design, implementation, & maintenance of Network & security systems. Keeping what's supposed to be private and proprietary safe. In addition to having a number of certifications, I'm rather skilled at managing security policy practices, including both development & validation. I managed a remote (virtual) team.",
+         tags: [mentorTags[0], mentorTags[5]],
+         topmate: "https://www.linkedin.com/in/muhammad-zunnurain-hussain-50641b61/?originalSubdomain=pk",
+    },
+    
     
     
   ];
