e02e522224a449c9bdf58406f87a39108f6b0daa
Adil Shehzad <adil.shehzad4146@gmail.com>
2024-04-12 23:59:44 +0500

    Update upcomingEvents.js

--- src/mocks/upcomingEvents.js
+++ src/mocks/upcomingEvents.js
@@ -1,12 +1,12 @@
 export const upcomingEvents = [
   
   {
-    name: "BUILD WITH AI LAHORE 2024",
+    name: "BUILD WITH AI- LAHORE 2024",
     category: ["Gemini", "Workshop"],
     image:
-      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/events/DALL%C2%B7E%202024-01-23%2023.15.31%20-%20An%20event%20thumbnail%20design%20for%20%27Python%20%20Cloud%20Synergy_%20Transformative%20Techniques%20with%20Google%20Gemini%20and%20Vertex%20AI%27%2C%20without%20any%20text.%20The%20design%20shoul_ayXg4O1.png",
-    date: "March 02, 2024",
-    link: "https://gdg.community.dev/events/details/google-gdg-cloud-lahore-presents-build-with-ai-lahore-2024/",
+      "https://i.ibb.co/hZqSR3B/1712630588413.jpg",
+    date: "April,20th 2024",
+    link: "https://gdg.community.dev/events/details/google-gdg-cloud-lahore-presents-build-with-ai-lahore/",
   },
 
   
